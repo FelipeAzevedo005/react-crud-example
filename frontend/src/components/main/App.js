f65f982d016f01563c9fc83b2b1ea46ffd85c918
Felipe Azevedo <felipe.azevedo005@protonmail.com>
2020-10-20 08:35:06 -0300

    Added Header component

--- frontend/src/components/main/App.js
+++ frontend/src/components/main/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./App.css"; 
 
+import Header from "../template/Header";
+
 function App() {
   return (
     <div className="App">
+      <Header title="Início" subtitle="Segundo Projeto do capítulo de React"/>
     </div>
   );
 }
