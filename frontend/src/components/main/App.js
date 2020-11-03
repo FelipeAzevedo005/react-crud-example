b50196ccadec743a1c971e2e30645e5bf91db786
Felipe Azevedo <felipe.azevedo005@protonmail.com>
2020-11-03 18:49:44 -0300

    Footer component added

--- frontend/src/components/main/App.js
+++ frontend/src/components/main/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import "./App.css"; 
+import Footer from "../template/Footer";
 
 import Main from "../template/Main";
 
 function App() {
     return (
-        <div className="App">
+        <div className="app">
             <Main icon="home" title="Início" 
                 subtitle="Segundo Projeto do capítulo de React" icon="home">
                 <div className="display-4">Bem Vindo!</div>
@@ -16,6 +17,8 @@ function App() {
                     de um cadastro desenvolvido em React!
                 </p>
             </Main>
+
+            <Footer />
         </div>
     );
 }
