e484a78a6eefc8deae84779506c7c2260f2f66b6
Felipe Azevedo <felipe.azevedo005@protonmail.com>
2020-11-04 18:58:05 -0300

    Home Component added

--- frontend/src/components/main/App.js
+++ frontend/src/components/main/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 
 import Logo from "../template/Logo";
 import Nav from "../template/Nav";
-import Main from "../template/Main";
+import Home from "../home/Home";
 import Footer from "../template/Footer";
 
 function App() {
@@ -13,15 +13,7 @@ function App() {
         <div className="app">
             <Logo />
             <Nav />
-            <Main icon="home" title="Início" 
-                subtitle="Segundo Projeto do capítulo de React" icon="home">
-                <div className="display-4">Bem Vindo!</div>
-                <hr/>
-                <p className="mb-0">Sistema para exemplificar a construção
-                    de um cadastro desenvolvido em React!
-                </p>
-            </Main>
-
+            <Home />
             <Footer />
         </div>
     );
