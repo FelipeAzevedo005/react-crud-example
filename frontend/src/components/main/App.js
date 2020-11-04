8735bfb9d8ec971c1fa1e2435eb02a546bee5c35
Felipe Azevedo <felipe.azevedo005@protonmail.com>
2020-11-04 11:38:24 -0300

    Grid layout added

--- frontend/src/components/main/App.js
+++ frontend/src/components/main/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import "./App.css"; 
-import Footer from "../template/Footer";
 
 import Logo from "../template/Logo";
 import Nav from "../template/Nav";
 import Main from "../template/Main";
+import Footer from "../template/Footer";
 
 function App() {
     return (
