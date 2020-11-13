1546eefa8a593b43bd21759c06e73be325c094c1
Felipe Azevedo <felipe.azevedo005@protonmail.com>
2020-11-13 10:14:58 -0300

    Anchor tags replaced by Link components for better fluidity in route changes

--- frontend/src/components/template/NavItem.jsx
+++ frontend/src/components/template/NavItem.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NavItem(props) {
     return (
-        <a href={props.href}>
+        <Link to={props.href}>
             <i className={`fa fa-${props.icon}`}></i> {props.text}
-        </a>
+        </Link>
     );
 }
 
