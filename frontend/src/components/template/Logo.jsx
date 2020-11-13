2ca01487aa2b94f470e21a229b428c31ac08967f
Felipe Azevedo <felipe.azevedo005@protonmail.com>
2020-11-13 19:09:26 -0300

    Added user form and users list on UserCRUD component

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Logo.css";
 import logoImg from "../../assets/imgs/logo.png";
 
 function Logo(props) {
     return (
         <aside className="logo">
-            <a href="/" className="logo">
+            <Link to="/" className="logo">
                 <img src={logoImg} alt="logo"/>
-            </a>
+            </Link>
         </aside>
     );
 }
